3e4011d6b541d2f18c05f62a47fbab85dc90730c
keren man <bebemau@gmail.com>
2015-01-22 18:51:37 -0800

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Man, Keren on 1/22/15.
+//  Copyright (c) 2015 UW PCE Apple Class. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ gitTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by Man, Keren on 1/22/15.
+//  Copyright (c) 2015 UW PCE Apple Class. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
